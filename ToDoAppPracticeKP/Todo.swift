7689cbca7b025e5e9002ff85dcbb2b1b142c0329
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-23 19:58:04 -0600

    --Added: custom color

--- ToDoAppPracticeKP/Todo.swift
+++ ToDoAppPracticeKP/Todo.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import SwiftData
+import SwiftUI
 
 @Model
 final class Todo {
@@ -39,5 +40,38 @@ class Tag {
     
     init(name: String) {
         self.name = name
+    }
+}
+
+struct RGBColor: Codable {
+    let red: Double
+    let green: Double
+    let blue: Double
+    let alpha: Double
+    
+    init(red: Double, green: Double, blue: Double, alpha: Double = 1.0) {
+        self.red = red
+        self.green = green
+        self.blue = blue
+        self.alpha = alpha
+    }
+    
+    init(_ color: Color) {
+        let uiColor = UIColor(color)
+        var red: CGFloat = 0.0
+        var green: CGFloat = 0.0
+        var blue: CGFloat = 0.0
+        var alpha: CGFloat = 0.0
+        
+        uiColor.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        self.red = Double(red)
+        self.green = Double(green)
+        self.blue = Double(blue)
+        self.alpha = Double(alpha)
+    }
+    
+    func toSwiftUIColor() -> Color {
+        return Color(red: red, green: green, blue: blue)
     }
 }
