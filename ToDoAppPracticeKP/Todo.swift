fe5043443a9262f555ea86c4788bbf3c99fdf31f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-23 19:05:38 -0600

    --Updating: to current spot of customizing boilerplate code for our custom ToDo app

--- ToDoAppPracticeKP/Todo.swift
+++ ToDoAppPracticeKP/Todo.swift
@@ -10,9 +10,20 @@ import SwiftData
 
 @Model
 final class Todo {
-    var timestamp: Date
     
-    init(timestamp: Date) {
-        self.timestamp = timestamp
+    @Attribute(.unique)
+    var creationDate: Date
+    var name: String
+    var isDone: Bool
+    var priority: Int
+    
+    @Attribute(.externalStorage)
+    var image: Data?
+    
+    init(name: String, isDone: Bool = false, priority: Int = 0) {
+        self.creationDate = Date()
+        self.name = name
+        self.isDone = isDone
+        self.priority = priority
     }
 }
