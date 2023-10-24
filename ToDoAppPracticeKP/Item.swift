ee37934a3fa561e9f8c2b0d2e091c84fd68d6cc6
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-23 18:47:21 -0600

    Initial Commit

---
+++ ToDoAppPracticeKP/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ToDoAppPracticeKP
+//
+//  Created by Mitch Andrade on 10/23/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ToDoAppPracticeKP/ToDoAppPracticeKPApp.swift
@@ -0,0 +1,32 @@
+//
+//  ToDoAppPracticeKPApp.swift
+//  ToDoAppPracticeKP
+//
+//  Created by Mitch Andrade on 10/23/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ToDoAppPracticeKPApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
