0adf30e23190a11302c899dceb6bbcf0bd4c34cb
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-24 08:02:57 -0600

    --Progress: start of new deep work session. Continuing to refactor and add more functionality to the todo app

---
+++ ToDoAppPracticeKP/DetailTodoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailTodoView.swift
+//  ToDoAppPracticeKP
+//
+//  Created by Mitch Andrade on 10/24/23.
+//
+
+import SwiftUI
+
+struct DetailTodoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailTodoView()
+}
